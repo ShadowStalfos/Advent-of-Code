a1a8919383b95c76b9c45005c4cb094eff79fe48
ShadowStalfos <thomas.komen@hotmail.com>
2023-12-05 21:09:00 +0100

    Removing prints from day 3.1

--- Day3/part1.py
+++ Day3/part1.py
@@ -1,7 +1,5 @@
 import numpy as np
 import re
-total_numbers = []
-total_re = []
 num_list = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 with open("./Day3/engine.txt") as f:
     engine = []
@@ -14,9 +12,6 @@ total_parts = 0
 with open("./Day3/engine.txt") as f:
     for n, line in enumerate(f):
         i = 0
-        finds = re.findall("\d+", line)
-        for x in finds:
-            total_re.append(x)
         while i < len(line):
             if line[i] in num_list: 
                 min_i =  max(i-1,0)
@@ -28,7 +23,6 @@ with open("./Day3/engine.txt") as f:
                 else:
                     num = line[min_i+1:max_i]
                 surrounding = engine[max(n-1, 0):n+2,min_i:max_i+1]
-                total_numbers.append(num)
                 for element in surrounding.flatten():
                     if (not element.isnumeric()) and element!=".":
                         total_parts += int(num)
@@ -38,13 +32,5 @@ with open("./Day3/engine.txt") as f:
             else:
                 i+=1
 
-total_numbers = np.array(total_numbers)
-
-for a, b in zip(total_re, total_numbers):
-    if a!=b:
-        print(a, b)
-
-print(len(total_numbers))
-print(len(total_re))
 print(total_parts)
 
