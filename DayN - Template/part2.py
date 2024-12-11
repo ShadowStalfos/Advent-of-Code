8558dfe6d214c2726c3c58af2e5d3552a4f036a7
ShadowStalfos <40166524+ShadowStalfos@users.noreply.github.com>
2024-12-11 16:26:19 +0100

    updated template to allow doubke digit days

--- DayN - Template/part2.py
+++ DayN - Template/part2.py
@@ -10,7 +10,12 @@ if TEST:
     with open(f"{os.path.split(os.path.abspath(__file__))[0]}\\example.txt", "r", encoding="utf-8") as f:
         data = f.read()
 else:
-    data = get_data(day=int(__file__.split("\\")[-2].split(" -")[0][-1]), year=2024)
+    day = __file__.split("\\")[-2].split(" -")[0]
+    if day[-2].isdigit():
+        day = day[-2:]
+    else:
+        day = day[-1]
+    data = get_data(day=day, year=2024)
 
 reports = [x.split(" ") for x in data.split("\n")]
 print(reports)
